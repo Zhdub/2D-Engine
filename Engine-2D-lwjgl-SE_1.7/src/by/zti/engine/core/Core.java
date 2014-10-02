2b3e504f466d15885498b17210b69378ba510d57
Cvazer <zhirnitoni@gmail.com>
2014-10-02 17:40:35 +0300

    All new Stuff

--- Engine-2D-lwjgl-SE_1.7/src/by/zti/engine/core/Core.java
+++ Engine-2D-lwjgl-SE_1.7/src/by/zti/engine/core/Core.java
@@ -1,8 +1,25 @@
 package by.zti.engine.core;
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.glViewport;
 
-import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -11,97 +28,56 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import org.newdawn.slick.opengl.Texture;
-import org.newdawn.slick.opengl.TextureLoader;
-import org.newdawn.slick.util.ResourceLoader;
 
-public class Core {
-	private static int WIDTH;
-	private static int HEIGTH;
+import by.zti.engine.Game;
+import by.zti.engine.Input;
+import by.zti.engine.core.render.Camera;
+import by.zti.engine.core.render.Renderer;
+import by.zti.engine.core.update.Updater;
 
+public class Core implements Runnable, Serializable{
+	private int WIDTH;
+	private int HEIGTH;
+	
+	private Renderer renderer = new Renderer(this);
+	private boolean isRunning; // true, если программа работает
+	private Camera camera; // Ссылка на объект отвечающий за игровую камеру
+	private ArrayList<GameObject> objects = new ArrayList<GameObject>(); // Ссылка на список объектов находящихся на обработке
+	
 	public Core(int width, int heigth) {
 		WIDTH = width;
 		HEIGTH = heigth;
-		initialiseDisplay();
-		initialiseGL(); 
-		initialiseGame();
-		gameLoop(); 
-	}
-
-	private static void initialiseGame() {
-		// TODO game intialisation
-	}
-
-	private static void gameLoop() {
-		while(!Display.isCloseRequested()){
-			glClear(GL_COLOR_BUFFER_BIT); //ќчистка буфера цветов, очистка экрана;
-			glLoadIdentity();
-			render();
-			Display.update();
-			Display.sync(60);
-		}
-		cleanUp();
-	}
-
-	private static void cleanUp() {
-		Display.destroy();
-		Keyboard.destroy();
-		Mouse.destroy();
-	}
-
-	private static void render() {
-		try {
-			Texture grass = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/grass2.png"));
 		
-		glPushMatrix();
-		glTranslatef(200, 200, 0);
-		glColor3f(1, 0, 0);
-		grass.bind();
-		glBegin(GL_QUADS);
-		{
-			
-			
-			
-
-			glVertex2f(0, 0);
-			glTexCoord2f(0, 0);
-			glVertex2f(0, 100);
-			glTexCoord2f(0, 1);
-			glVertex2f(100, 100);
-			glTexCoord2f(1, 1);
-			glVertex2f(100, 0);
-			glTexCoord2f(1, 0);
-			
-		}
-		glEnd();
-		glPopMatrix();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+		new Thread(this).start();
 		
-
-	private static void update() {
+	}
 	
+	public void update(){
+		for(GameObject object: objects){
+			object.update();
+		}
 	}
-
-
-	private static void initialiseGL() {
-		glEnable(GL_TEXTURE_2D);
-		glClearColor(0, 0, 0, 0);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glViewport(0,0,Display.getWidth(),Display.getHeight());
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(0, Display.getWidth(), 0, Display.getHeight(),-1,1);
-		glMatrixMode(GL_MODELVIEW);
-		glDisable(GL_DEPTH_TEST);
-//		glClearColor(0, 0, 0, 0);
+	
+	public void render(){ 
+		camera.update();
+		for(GameObject object: objects){
+			if(object.getRender_layer()==0){
+				object.render();
+			}
+		}
+		for(GameObject object: objects){
+			if(object.getRender_layer()==1){
+				object.render();
+			}
+		}
+		for(GameObject object: objects){
+			if(object.getRender_layer()==2){
+				object.render();
+			}
+		}
 	}
-
-	private static void initialiseDisplay() {
+	
+ 	public void initialiseDisplay() {
 		try {
 			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGTH));
 			Display.create();
@@ -111,8 +87,62 @@ public class Core {
 		} catch (LWJGLException e) {
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
+	}
 	
-}
+	public void initialiseGL() {
+		glEnable(GL_TEXTURE_2D);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glViewport(0,0,Display.getWidth(),Display.getHeight());
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, Display.getWidth(), 0, Display.getHeight(),-1,1);
+		glMatrixMode(GL_MODELVIEW);
+		glDisable(GL_DEPTH_TEST);
+		glClearColor(0, 0, 0, 0);
+	}
 
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		initialiseDisplay();
+		initialiseGL();
+		Textures.loadTextures();
+		while(!Display.isCloseRequested()){
+			if(Game.isObjectsChanged()){
+				objects = Game.getObjects();
+				Game.setObjectsChanged(false);
+			}
+			glClear(GL_COLOR_BUFFER_BIT); //ќчистка буфера цветов, очистка экрана;
+			glLoadIdentity();
+			Input.update();
+			update();
+			render();
+//			renderer.tick(this);
+			Display.update();
+			Display.sync(60);
+		}
+	}
+	
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+	public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+	}
+
+	public Camera getCamera() {
+		return camera;
+	}
+
+	public void setCamera(Camera camera) {
+		this.camera = camera;
+	}
 
 }
--- Engine-2D-lwjgl-SE_1.7/src/by/zti/game/MainCopmonent.java
+++ Engine-2D-lwjgl-SE_1.7/src/by/zti/game/MainCopmonent.java
@@ -1,11 +1,70 @@
 package by.zti.game;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import by.zti.SerializationManager;
+import by.zti.engine.Game;
+import by.zti.engine.GameMap;
 import by.zti.engine.core.Core;
+import by.zti.engine.core.GameObject;
 
 public class MainCopmonent {
+	public static Core core;
+	public static GameMap map = new GameMap();
+	private static ArrayList<GameObject> current_objects = new ArrayList<GameObject>();
+	private static WIP wip;
+	private static GameCamera camera;
 
 	public static void main(String[] args) {
-		Core core = new Core(800, 600);
+//		
+//		map = (GameMap) SerializationManager.deSerializeData("Map", "gmp", "");
+//		for(GameObject object: map.getAll()){
+//			if(object.getClass().getSimpleName().contains("WIP")){
+//				wip = (WIP) object;
+//			}
+//		}
+//		
+//		core = new Core(800, 600);
+//		camera = new GameCamera((800 / 2) - 20, (600 / 2) - 20, 4, 1);
+//		core.setCamera(camera);
+//		camera.bind(wip);
+//		
+//		for(GameObject object: map.getAll()){
+//			Game.addObject(object);
+//		}
+		initialise();
+		addObjectsToGame();
+
+		
+		
+//		Scanner scn = new Scanner(System.in);
+//		scn.nextLine();
+		
+//		SerializationManager.serializeData(map, "Map", "gmp", "");
 	}
 
+	public static void initialise() {
+		core = new Core(800, 600);
+		camera = new GameCamera((800 / 2) - 20, (600 / 2) - 20, 4, 1);
+		Stone stone = new Stone(40, 40, core);
+		Stone stone2 = new Stone(40, 120, core);
+		WIP wip = new WIP(200, 200, core);
+		camera.bind(wip);
+		core.setCamera(camera);
+		current_objects.add(stone);
+		current_objects.add(stone2);
+		current_objects.add(wip);
+	}
+
+	public static void addObjectsToGame() {
+		for (GameObject object : current_objects) {
+			Game.addObject(object);
+		}
+		for (int i = 0; i < 20; i++) {
+			for (int k = 0; k < 15; k++) {
+				Game.addObject(new Grass(i * 40, k * 40, core));
+			}
+		}
+	}
 }
